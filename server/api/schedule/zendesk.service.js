abb0b218b64b2c9fb73d94677da1e10c45159373
Ricardo Lara <riclara@gmail.com>
2017-12-05 17:28:38 -0300

    partial commit

--- server/api/schedule/zendesk.service.js
+++ server/api/schedule/zendesk.service.js
@@ -5,25 +5,30 @@ const dbService = require("../../services/db.service"),
   config = require("../../config/environment"),
   schedule = require('node-schedule'),
   log = require("../../services/log.service"),
-  axios = require("axios");
+  axios = require("axios"),
+  bugsnag = require("bugsnag");
 
-var scheduler = null;
-var isRunning = false;
+var scheduler = {
+  tickets: null,
+  events: null
+};
+var isTicketsRunning, isEventsRunning = false;
 
 function importZendeskTickets() {
-  var rule = new schedule.RecurrenceRule();
-  rule.minute = 60;
-  scheduler = schedule.scheduleJob(rule, function () {
-    if (!isRunning) {
-      isRunning = true;
+  let colName = "zendesk_tickets";
+  let rule = new schedule.RecurrenceRule();
+  rule.minute = 25;
+  scheduler.tickets = schedule.scheduleJob(rule, function () {
+    if (!isTicketsRunning) {
+      isTicketsRunning = true;
       dbService.connect((err, db) => {
-        let collection = db.collection("zendesk_tickets");
-        db.collection("zendesk_tickets").find().sort({ generated_timestamp: -1 }).limit(1).toArray((err, result) => {
+        let collection = db.collection(colName);
+        collection.find().sort({ generated_timestamp: -1 }).limit(1).toArray((err, result) => {
           let startDate = 1420070400;
           if (result.length) {
             startDate = result[0].generated_timestamp
           }
-          getTickets(collection, startDate);
+          retrieveIncremental(collection, "tickets", "organizations,comment_count", startDate, parseTicket, isTicketsRunning);
         });
       });
     } else {
@@ -73,6 +78,82 @@ function getTickets(collection, startTime) {
     log.error(error);
   });
 
+}
+
+function retrieveIncremental(collectionDB, item, include, startTime, parseFunction, isRunning) {
+  log.info(`retrieveIncremental ${item} - ${startTime}`);
+  axios({
+    method: 'get',
+    url: `${config.zendesk.url}/api/v2/incremental/${item}.json?include=${include}&start_time=${startTime}`,
+    auth: {
+      username: config.zendesk.username,
+      password: config.zendesk.password
+    }
+  }).then(function (response) {
+    let data = response.data;
+    let isLastPage = data.tickets.length <= 1;
+
+    if (!isLastPage) {
+      let nextStartDate = Number(data.next_page.split("=")[1]);
+      persistIncremental(data[item], collectionDB, parseFunction, (err, result) => {
+        if(err){
+          bugsnag.notify(err);
+          log.error(err);
+          isRunning = false;
+          return;
+        }
+        log.info('persist result: ' + result)
+        retrieveIncremental(collectionDB, item, include, nextStartDate, parseFunction);
+      });
+      
+    } else {
+      isRunning = false;
+      log.info("##end##")
+    }
+
+  }).catch(function (error) {
+    bugsnag.notify(error);
+    log.error(error);
+  });
+
+}
+
+function persistIncremental(incrementals, collection, parseFunction, cb){
+  let batch = collection.initializeOrderedBulkOp();
+  for (let item of incrementals) {
+    item = parseFunction(item);
+    batch.find({ id: item.id, }).upsert().updateOne({ $setOnInsert: item });
+  }
+  batch.execute((err, exec) => {
+    if (err) {
+      log.error(err);
+      cb(err);
+      return;
+    }
+    cb(null, exec);
+  });
+}
+
+function parseTicket(ticket){
+  if(ticket.created_at){
+    ticket.created_at = new Date(ticket.created_at)
+  }
+  if(ticket.updated_at){
+    ticket.updated_at = new Date(ticket.updated_at)
+  }
+  return ticket;
+}
+
+function parseEvent(event){
+  if(event.created_at){
+    event.created_at = new Date(event.created_at)
+  }
+  for(let childEvent of event.child_events){
+    if(childEvent.created_at){
+      childEvent.created_at = new Date(event.created_at)
+    }
+  }
+  return event;
 }
 
 module.exports = {
