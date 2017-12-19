9e7e81cec4cd3c8ec09133f1676874dc9018fe98
Ricardo Lara <riclara@gmail.com>
2017-12-19 16:08:10 -0500

    fix update zendesk records

--- server/api/schedule/zendesk.service.js
+++ server/api/schedule/zendesk.service.js
@@ -33,7 +33,7 @@ function importZendesk(item, include) {
         collection.find().sort(filter).limit(1).toArray((err, result) => {
           let start = 1420070400;
           if (result.length) {
-            start = startTime[item].get(result[0]) - 5184000;
+            start = startTime[item].get(result[0]);
           }
           let includeQuery = include ? `include=${include}&` : "";
           let url = `${config.zendesk.url}/api/v2/incremental/${item}.json?${includeQuery}start_time=${start}`
@@ -89,7 +89,7 @@ function persistIncremental(incrementals, item, collection, cb) {
   let batch = collection.initializeOrderedBulkOp();
   for (let ele of incrementals) {
     ele = parseMethod[item](ele);
-    batch.find({ id: ele.id, }).upsert().updateOne({ $setOnInsert: ele });
+    batch.find({ id: ele.id }).upsert().updateOne({ $set: ele });
   }
   batch.execute((err, result) => {
     if (err) {
